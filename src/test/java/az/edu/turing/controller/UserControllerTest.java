2964b59f82d2e01a3d8bc7dd05aca49504c80557
Javad <javadnazirli24@gmail.com>
2025-01-31 00:44:01 +0400

    UserControllerTest was added.

---
+++ src/test/java/az/edu/turing/controller/UserControllerTest.java
@@ -0,0 +1,22 @@
+package az.edu.turing.controller;
+
+import az.edu.turing.service.UserService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockitoBean
+    private UserService userService;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+}
