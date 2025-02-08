c2b6f74fe24cb9fad765ae6dc6839ce2733186c5
Javad <javadnazirli24@gmail.com>
2025-02-08 14:50:25 +0400

    Json files was fixed

--- src/test/java/az/edu/turing/common/JsonFiles.java
+++ src/test/java/az/edu/turing/common/JsonFiles.java
@@ -8,7 +8,7 @@ public interface JsonFiles {
     String PAGEABLE_RESPONSE_DTO_JSON = "pageable-response-dto.json";
     String CREATE_USER_REQUEST_JSON = "create-user-request.json";
     String UPDATE_USER_REQUEST_JSON = "update-user-request.json";
-    String USER_DTO_JSON = "user-dto.json" ;
+    String USER_DTO_JSON = "user-dto.json";
 
 
 }
